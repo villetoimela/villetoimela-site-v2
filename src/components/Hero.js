9cd629a99e12c0bdbd493b29f7e257187ad3e88f
villetomela <ville.toimela@bermuda.fi>
2023-09-15 22:44:31 +0300

    Make functionality for scroll vertically etc. Also added background noise effect

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <>
+    <div className="bg"></div>
+        <header className="hero__header">
+          <h1>Ville</h1>
+          <h1>Toimela</h1>
+          <h2>Web Developer</h2>
+      </header>
+      </>
+    
+  );
+}
+
+export default Hero;
