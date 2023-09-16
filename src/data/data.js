ec6c49821876c70bf7d4388848d273411ddf0633
Ville Toimela <ville.toimela@bermuda.fi>
2023-09-16 22:40:49 +0300

    Addded new component and data.js files. codded and style them like CRAZY

---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        text: "I am a passionate web developer with some experience in creating diverse websites and web applications using various technologies and programming languages.",
+        image: "project-1.jpg",
+        class: "perspective-left",
+        url: "#",
+    },
+    {
+        text: "Tää on toka",
+        image: "project-2.jpg",
+        class: "perspective-right",
+        url: "#",
+    },
+    {
+        text: "Tää on kolmas",
+        image: "project-3.jpg",
+        class: "perspective-left third",
+        url: "#",
+    },    
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import styles from '@/styles/Home.module.css'
 import Hero from '@/components/Hero'
 import ScrollSection from '@/components/ScrollSection'
 import Footer from '@/components/Footer'
+import Projects1 from '@/components/Projects1'
+import Projects2 from '@/components/Projects2'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +15,9 @@ export default function Home() {
     <>
     <Hero />
     <ScrollSection />
+    <Projects1 />
     <Footer />
+    <Projects2 />
     </>
   )
 }
