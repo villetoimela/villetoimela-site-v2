89085bc54aad993d47cf53608786ef29047bff3c
villetomela <ville.toimela@bermuda.fi>
2023-09-16 17:11:57 +0300

    Coded and styled hero section. Added asterisk svg.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,24 @@ import React from "react";
 function Hero() {
   return (
     <>
-    <div className="bg"></div>
-        <header className="hero__header">
-          <h1>Ville</h1>
-          <h1>Toimela</h1>
-          <h2>Web Developer</h2>
+      <div className="bg"></div>
+
+      <header className="hero__header">
+        <h1 className="hero-first-text">VILLE</h1>
+        <div className="asterisk">
+          <img src="/asterisk.svg" alt="Asterisk" />
+        </div>
+        <h1 className="hero-second-text">TOIMELA</h1>
+        <h2 className="hero-third-text">Web Developer</h2>
+        <div className="hero-info-wrapper">
+          <p className="hero-info-text">
+            I am a web developer with some experience in creating diverse websites 
+            and web applications using various technologies and programming languages.
+          </p>
+        </div>
+        
       </header>
-      </>
-    
+    </>
   );
 }
 
