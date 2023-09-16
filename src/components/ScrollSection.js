92dd1c21ae2dcc15bff000da06e97e4380fcccfd
Ville Toimela <ville.toimela@bermuda.fi>
2023-09-16 23:37:39 +0300

    Moded scroll-section. Tested some weird layout things! WIP

--- src/components/ScrollSection.js
+++ src/components/ScrollSection.js
@@ -43,18 +43,36 @@ function ScrollSection() {
       the animation should alway be two separated refs */}
       <div ref={triggerRef}>
         <div ref={sectionRef} className="scroll-section-inner">
-          <div className="scroll-section">
-            <h3>Section 1</h3>
+          <div className="scroll-section ">
+            <div className="section-1">
+              <h2>Who am i</h2>
+              <h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</h3>
+              <img src="/project-1.jpg" />
+            </div>
+            
           </div>
-          <div className="scroll-section">
-            <h3>Section 2</h3>
+          
+          <div className="scroll-section ">
+            <div className="section-2">
+              <h2>Who am i</h2>
+              <h3>Section 1</h3>
+            </div>
           </div>
-          <div className="scroll-section">
-            <h3>Section 3</h3>
+
+          <div className="scroll-section ">
+            <div className="section-3">
+              <h2>Who am i</h2>
+              <h3>Section 1</h3>
+            </div>
           </div>
-          <div className="scroll-section">
-            <h3>Section 4</h3>
+
+          <div className="scroll-section ">
+            <div className="section-4">
+              <h2>Who am i</h2>
+              <h3>Section 1</h3>
+            </div>
           </div>
+
         </div>
       </div>
     </section>
