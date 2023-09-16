f2d277822a4bfd34bfe837fd209911a1ee419ede
villetomela <ville.toimela@bermuda.fi>
2023-09-16 22:40:49 +0300

    Addded new component and data.js files. codded and style them like CRAZY

---
+++ src/data/data2.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        text: "Tää on data 2 eka",
+        image: "project-1.jpg",
+        class: "perspective-left",
+        url: "#",
+    },
+    {
+        text: "Tää on data 2 toka",
+        image: "project-2.jpg",
+        class: "perspective-right",
+        url: "#",
+    },
+    {
+        text: "Tää on data 2 kolmas",
+        image: "project-3.jpg",
+        class: "perspective-left third",
+        url: "#",
+    },    
+]
