bc6c94a9cec776d38e5bbc21a1ef36380c6d66f6
villetomela <ville.toimela@bermuda.fi>
2023-09-20 21:14:43 +0300

    Added preloader, dis was coooooool af

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,38 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence } from 'framer-motion';
+
 import Hero from '@/components/Hero'
 import ScrollSection from '@/components/ScrollSection'
 import Footer from '@/components/Footer'
 import Projects1 from '@/components/Projects1'
 import Projects2 from '@/components/Projects2'
 import TitleSection from '@/components/TitleSection'
-import InfiniteScroll from '@/components/InfiniteScroll'
-
-const inter = Inter({ subsets: ['latin'] })
+import Preloader from '@/components/Preloader'
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2500)
+      }
+    )()
+  }, [])
+
   return (
-    <>
+    <main>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
     <Hero />
     <ScrollSection />
     <TitleSection title ="SOME WORDPRESS PROJECTS" />
@@ -23,6 +41,6 @@ export default function Home() {
     <Projects2 />
     <TitleSection title ="MORE PROJECTS IN GITHUB" link="https://github.com/villetoimela" linkTitle="> github" />
     <Footer />
-    </>
+    </main>
   )
 }
