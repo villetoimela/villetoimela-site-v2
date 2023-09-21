4bfe81240bcc1d88c6dab54f471a811ad8c273b5
Ville Toimela <ville.toimela@bermuda.fi>
2023-09-21 22:21:10 +0300

    Added unic client made svg's and used couple of those in ScrollSection. Styled ScrollSection responsiveness too

--- src/components/ScrollSection.js
+++ src/components/ScrollSection.js
@@ -60,14 +60,14 @@ function ScrollSection() {
                 <br/> <br/>
                 I've logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate atleast an equal number of hours in coding and continue expanding my knowledge.
               </h3>
-              <img src="/abstract-bw1.jpg" />
+              <img src="/kissa-sivulle.svg" />
             </div>
             
           </div>
           
           <div className="scroll-section second-section">
             <div className="section-2">
-              <img src="/abstract-bw2.jpg" />
+              <img src="/läppäri-sivulle.svg" />
               <h2>What I do</h2>
               <h3>
                 After an intensive two-year Software Developer training, I had the privilege of working at one of Finland's most influential marketing agencies. There, I've been involved in creating various websites tailored to meet the specific needs of clients. Most of these projects involved building custom WordPress themes. In addition to coding, this role has provided me with invaluable experience in teamwork and interacting effectively with clients.
@@ -98,7 +98,7 @@ function ScrollSection() {
             <div className="section-4">
               <h2>What next?</h2>
               <h3>All in all, I aim to be a versatile software developer, and I possess a strong passion for embracing new technologies! I plan to continue growing in software development even outside of work, and my ultimate goal is to someday be DA TECH GOD.</h3>
-              <img src="/abstract-bw5.jpg" />
+              <img src="/pilvi-ukkeli-sivulle.svg" />
             </div>
           </div>
 
