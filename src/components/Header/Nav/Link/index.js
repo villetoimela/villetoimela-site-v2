fc83cdd505443eafe960cb7605e5904514470385
Ville Toimela <ville.toimela@bermuda.fi>
2023-09-23 18:27:31 +0300

    Finally fucking did it. I hate myself because i cannot just pass props as normal element id="" because ScrollSection broke. Now i had to do that with UseRef and that was weird to use. STILL I DID IT yeehaw. fuck that take too much time tho

--- src/components/Header/Nav/Link/index.js
+++ src/components/Header/Nav/Link/index.js
@@ -1,15 +1,27 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { motion } from 'framer-motion';
 import { slide, scale } from '../../anim';
 
-export default function Index({data, isActive, setSelectedIndicator}) {
-  
-    const { title, href, index} = data;
-  
-    return (
-      <motion.div className="nav-link" onMouseEnter={() => {setSelectedIndicator(href)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
-        <motion.div variants={scale} animate={isActive ? "open" : "closed"} className="nav-indicator"></motion.div>
-        <Link href={href}>{title}</Link>
-      </motion.div>
-    )
-  }
+export default function Index({ data, isActive, setSelectedIndicator, onClick }) {
+  const { title, href, index } = data;
+
+  return (
+    <motion.div
+      className="nav-link"
+      onMouseEnter={() => { setSelectedIndicator(href) }}
+      custom={index}
+      variants={slide}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+    >
+      <motion.div variants={scale} animate={isActive ? "open" : "closed"} className="nav-indicator"></motion.div>
+      <NextLink href={href} passHref>
+        <div onClick={(e) => {
+          e.preventDefault();
+          if (onClick) onClick(e); 
+        }}>{title}</div>
+      </NextLink>
+    </motion.div>
+  );
+}
