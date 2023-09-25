21514b64f20a994f75990fb79ec2e963fcd2965a
villetomela <ville.toimela@bermuda.fi>
2023-09-25 21:17:29 +0300

    Added new component "ScrollSectionShort". coded and styled it

--- src/components/ScrollSectionShort.js
+++ src/components/ScrollSectionShort.js
@@ -1,9 +1,10 @@
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import InfiniteScroll from "./InfiniteScroll";
 
 
-function ScrollSection() {
+function ScrollSectionShort() {
   const sectionRef = useRef(null);
   const triggerRef = useRef(null);
 
@@ -16,13 +17,13 @@ function ScrollSection() {
         translateX: 0,
       },
       {
-        translateX: "-200vw",
+        translateX: "-100vw",
         ease: "none",
         duration: 1,
         scrollTrigger: {
           trigger: triggerRef.current,
           start: "top top",
-          end: "2000 top",
+          end: "1000 top",
           scrub: 0.6,
           pin: true,
         },
@@ -48,6 +49,16 @@ function ScrollSection() {
       yoyo: true, 
       ease: "power1.inOut" 
     });
+
+    const pilviukkeliImg = document.querySelector('.pilviukkeli-img');
+    gsap.to(pilviukkeliImg, {
+      y: -20, 
+      rotation: 3, 
+      duration: 3, 
+      repeat: -1, 
+      yoyo: true,
+      ease: "power1.inOut" 
+    });
     
     return () => {
       {/* A return function for killing the animation on component unmount */ }
@@ -63,7 +74,7 @@ function ScrollSection() {
       {/* The div below act just as a trigger. As the doc suggests, the trigger and 
       the animation should alway be two separated refs */}
       <div ref={triggerRef}>
-        <div ref={sectionRef} className="scroll-section-inner">
+        <div ref={sectionRef} className="short-scroll-section-inner">
         
         <svg className="mutkitteleva-viiva" width="600vw" height="auto" viewBox="0 0 4000 100">
           <path className="path" d="M0,-160 C500,-300 1000,200 1500,-50 C2000,-200 2500,400 3000,-50 C3500,-400 4000,100" fill="none" stroke="var(--red-color)" strokeWidth="5" />
@@ -74,52 +85,33 @@ function ScrollSection() {
         </svg>
 
           <div className="scroll-section first-section">
-            <div className="section-1">
-              <h2>Who am i</h2>
+            <div className="short-section-1">
+              <h2>SOME MORE ABOUT ME</h2>
               <h3>
                 Hello! I'm a 28-years-old eager Web Developer with about three years of active coding experience. While I've always been intrigued by programming, I officially embarked on my learning journey only three years ago. During this time, I learned many different skills and made some digital ideas real!
                 <br/> <br/>
                 I've logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate atleast an equal number of hours in coding and continue expanding my knowledge.
               </h3>
-              <img src="/kissa-sivulle.svg" className="kissa-img" />
+              <img src="/pilvi-ukkeli-sivulle.svg" className="pilviukkeli-img" />
 
             </div>
             
           </div>
           
-          <div className="scroll-section second-section">
-            <div className="section-2">
-              <img src="/läppäri-sivulle.svg" className="lappari-img" />
-              <h2>What I do</h2>
-              <h3>
-                After an intensive two-year Software Developer training, I had the privilege of working at one of Finland's most influential marketing agencies. There, I've been involved in creating various websites tailored to meet the specific needs of clients. Most of these projects involved building custom WordPress themes. In addition to coding, this role has provided me with invaluable experience in teamwork and interacting effectively with clients.
-              <br/> <br/>
-                During my studies, a fellow student and I co-founded a company. Through this venture, we had the chance to work on several exciting projects! If you're interested, check out <a href='https://www.hiisi.digital/'>hiisi.digital</a>.
-              </h3>
+          <div className="scroll-section fourth-section">
+            <div className="section-4">
+              <h2>What next?</h2>
+              <h3>All in all, I aim to be a versatile software developer, and I possess a strong passion for embracing new technologies! I plan to continue growing in software development even outside of work, and my ultimate goal is to someday be DA TECH GOD.</h3>
               
               
             </div>
+            <InfiniteScroll />
           </div>
-
-          <div className="scroll-section third-section">
-            <div className="section-3">
-              <h2>What i know</h2>
-              <h3>For me, it's not just about mastering specific programming languages or technologies; it's about embracing the entirety of web development. I've garnered a wealth of experience across a range of technologies over the years.</h3>
-            </div>
-            <div className="section-3">
-              <h2 className="title">Technologies and tools</h2>
-              <h3>
-                Over the years, my journey as a web developer has been both vast and fulfilling. I've mastered the art of creating seamless user interfaces using tools like HTML, CSS, JavaScript, and React. Venturing further, PHP, Next.js, TailwindCSS, and WordPress became trusted companions in my toolkit, allowing me to shape visions into digital realities.
-              <br/> <br/>
-                Of course, I'm accustomed to using version control in my projects, and in addition, I've had a glimpse into the world of databases and servers.
-              </h3>
-            </div>
-          </div>
-
+          
         </div>
       </div>
     </section>
   );
 }
 
-export default ScrollSection;
+export default ScrollSectionShort;
