ee9eeb9adc2f12d5d3169e4abadd2bbe4a78889c
Ville Toimela <ville.toimela@bermuda.fi>
2023-12-08 15:23:02 +0200

    Added FloatingGallery component

---
+++ src/data/data3.js
@@ -0,0 +1,21 @@
+import floating1 from '../../public/images/project-tammermatic-desktop.png';
+import floating2 from '../../public/images/project-tammermatic-desktop.png';
+import floating3 from '../../public/images/project-tammermatic-desktop.png';
+import floating4 from '../../public/images/project-tammermatic-desktop.png';
+import floating5 from '../../public/images/project-tammermatic-desktop.png';
+import floating6 from '../../public/images/project-tammermatic-desktop.png';
+import floating7 from '../../public/images/project-tammermatic-desktop.png';
+import floating8 from '../../public/images/project-tammermatic-desktop.png';
+import floating9 from '../../public/images/project-tammermatic-desktop.png';
+
+export { 
+    floating1, 
+    floating2, 
+    floating3, 
+    floating4, 
+    floating5, 
+    floating6, 
+    floating7, 
+    floating8,
+    floating9
+}
