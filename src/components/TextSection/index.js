40fa3c7748774532a53f72c17acf0de4f0276295
Ville Toimela <ville.toimela@bermuda.fi>
2023-12-08 13:17:40 +0200

    Modded TextSection, added gsap scrolltrigger animations

--- src/components/TextSection/index.js
+++ src/components/TextSection/index.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 
 export default function Index(props) {
     const { title, content, swap } = props;
+    const leftSectionRef = useRef(null);
+    const rightSectionRef = useRef(null);
+
+    useEffect(() => {
+        // Vasemman osion animaatio, esiin alhaalta vasemmalta
+        gsap.fromTo(leftSectionRef.current, 
+            { y: 80, x: -100, opacity: 0, scale: 0.9 }, // Alkutila
+            { y: 0, x: 0, opacity: 1, scale: 1,          // Lopputila
+              duration: 1.5,
+              ease: "power2.out", // Sujuvampi ja vähemmän pompottava liike
+              scrollTrigger: {
+                trigger: leftSectionRef.current,
+                start: "top bottom",
+                end: "top center",
+                scrub: 1,
+              }
+            }
+        );
+
+        // Oikean osion animaatio, esiin alhaalta oikealta
+        gsap.fromTo(rightSectionRef.current, 
+            { y: 80, x: 100, opacity: 0, scale: 0.9 }, // Alkutila
+            { y: 0, x: 0, opacity: 1, scale: 1,         // Lopputila
+              duration: 1.5,
+              ease: "power2.out", // Sujuvampi ja vähemmän pompottava liike
+              scrollTrigger: {
+                trigger: rightSectionRef.current,
+                start: "top bottom",
+                end: "top center",
+                scrub: 1,
+              }
+            }
+        );
+    }, []);
 
     const leftSection = swap ? (
-        <div class="left-section">
+        <div className="left-section" ref={leftSectionRef}>
             <p>{content}</p>
         </div>
     ) : (
-        <div class="left-section">
+        <div className="left-section" ref={leftSectionRef}>
             <h4>{title}</h4>
         </div>
     );
 
     const rightSection = swap ? (
-        <div class="right-section">
+        <div className="right-section" ref={rightSectionRef}>
             <h4>{title}</h4>
         </div>
     ) : (
-        <div class="right-section">
+        <div className="right-section" ref={rightSectionRef}>
             <p>{content}</p>
         </div>
     );
