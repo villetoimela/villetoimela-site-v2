b9dd5370eb958fc163bd0120a22c443ae20b211b
villetomela <ville.toimela@bermuda.fi>
2024-01-08 11:43:41 +0200

    Minor changes here and there

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,13 +65,19 @@ export default function Home() {
       <Hero ref={home} />
       <TextSection ref={textsection}
         title = "Who am i"
-        content ="Hello! I'm a 28-years-old eager Web Developer with about three years of active coding experience. While I've always been intrigued by programming, I officially embarked on my learning journey only three years ago. During this time, I learned many different skills and made some digital ideas real! I've logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate atleast an equal number of hours in coding and continue expanding my knowledge."
+        content ="Hello! I'm a 28-years-old eager Web Developer with about four years of active coding experience. While I've always been intrigued by programming, I officially embarked on my learning journey only four years ago. During this time, I learned many different skills and made some digital ideas real! I've logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate atleast an equal number of hours in coding and continue expanding my knowledge."
       />
       <TextSection
         title = "What i do"
-        content ="After an intensive two-year Software Developer training, I had the privilege of working at one of Finland's most influential marketing agencies. There, I've been involved in creating various websites tailored to meet the specific needs of clients. Most of these projects involved building custom WordPress themes. In addition to coding, this role has provided me with invaluable experience in teamwork and interacting effectively with clients.
-          During my studies, a fellow student and I co-founded a company. Through this venture, we had the chance to work on several exciting projects! If you're interested, check out <a href='https://www.hiisi.digital/'>hiisi.digital</a>."
-      />
+        content={
+          <>
+            After an intensive two-year Software Developer training, I had the privilege of working at one of Finland's most influential marketing agencies. There, I've been involved in creating various websites tailored to meet the specific needs of clients. Most of these projects involved building custom WordPress themes. In addition to coding, this role has provided me with invaluable experience in teamwork and interacting effectively with clients.
+            <br />
+            <br />
+            During my studies, my brother and I co-founded a company. Through this venture, we had the chance to work on several exciting projects! If you're interested, check out <a href="https://www.hiisi.digital/" target="_blank" rel="noopener noreferrer">hiisi.digital</a>.
+          </>
+        }
+        />
       <TitleSection title ="Sneak peek to some of my projects" />
       <WorksPeek />
       <TitleSection textAlignRight={true} title ="Link to " link="https://github.com/villetoimela" linkTitle="> github" />
