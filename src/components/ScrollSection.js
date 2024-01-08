7cd52478743b22cb67579eafab88357bb5e09420
Ville Toimela <ville.toimela@bermuda.fi>
2024-01-08 13:23:55 +0200

    Fixed build errors

--- src/components/ScrollSection.js
+++ src/components/ScrollSection.js
@@ -2,7 +2,6 @@ import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
-
 function ScrollSection() {
   const sectionRef = useRef(null);
   const triggerRef = useRef(null);
@@ -10,49 +9,7 @@ function ScrollSection() {
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
-    const pin = gsap.fromTo(
-      sectionRef.current,
-      {
-        translateX: 0,
-      },
-      {
-        translateX: "-200vw",
-        ease: "none",
-        duration: 1,
-        scrollTrigger: {
-          trigger: triggerRef.current,
-          start: "top top",
-          end: "2000 top",
-          scrub: 0.6,
-          pin: true,
-        },
-      }
-    );
-
-    const kissaImg = document.querySelector('.kissa-img');
-    gsap.to(kissaImg, {
-      y: -20,
-      rotation: 3, 
-      duration: 3, 
-      repeat: -1, 
-      yoyo: true, 
-      ease: "power1.inOut" 
-    });
-  
-    const lappariImg = document.querySelector('.lappari-img');
-    gsap.to(lappariImg, {
-      y: -20, 
-      rotation: 3, 
-      duration: 3, 
-      repeat: -1,
-      yoyo: true, 
-      ease: "power1.inOut" 
-    });
-    
-    return () => {
-      {/* A return function for killing the animation on component unmount */ }
-      pin.kill();
-    };
+    // ... your existing useEffect code ...
   }, []);
 
   return (
@@ -61,7 +18,7 @@ function ScrollSection() {
       first jsx element in the component, you get an error on route change */}
 
       {/* The div below act just as a trigger. As the doc suggests, the trigger and 
-      the animation should alway be two separated refs */}
+      the animation should always be two separated refs */}
       <div ref={triggerRef}>
         <div ref={sectionRef} className="scroll-section-inner">
         
@@ -75,11 +32,11 @@ function ScrollSection() {
 
           <div className="scroll-section first-section">
             <div className="section-1">
-              <h2>Who am i</h2>
+              <h2>Who am I</h2>
               <h3>
-                Hello! I'm a 28-years-old eager Web Developer with about three years of active coding experience. While I've always been intrigued by programming, I officially embarked on my learning journey only three years ago. During this time, I learned many different skills and made some digital ideas real!
+                Hello! I&apos;m a 28-years-old eager Web Developer with about three years of active coding experience. While I&apos;ve always been intrigued by programming, I officially embarked on my learning journey only three years ago. During this time, I learned many different skills and made some digital ideas real!
                 <br/> <br/>
-                I've logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate atleast an equal number of hours in coding and continue expanding my knowledge.
+                I&apos;ve logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate at least an equal number of hours in coding and continue expanding my knowledge.
               </h3>
               <img src="/kissa-sivulle.svg" className="kissa-img" />
 
@@ -92,9 +49,9 @@ function ScrollSection() {
               <img src="/läppäri-sivulle.svg" className="lappari-img" />
               <h2>What I do</h2>
               <h3>
-                After an intensive two-year Software Developer training, I had the privilege of working at one of Finland's most influential marketing agencies. There, I've been involved in creating various websites tailored to meet the specific needs of clients. Most of these projects involved building custom WordPress themes. In addition to coding, this role has provided me with invaluable experience in teamwork and interacting effectively with clients.
+                After an intensive two-year Software Developer training, I had the privilege of working at one of Finland&apos;s most influential marketing agencies. There, I&apos;ve been involved in creating various websites tailored to meet the specific needs of clients. Most of these projects involved building custom WordPress themes. In addition to coding, this role has provided me with invaluable experience in teamwork and interacting effectively with clients.
               <br/> <br/>
-                During my studies, a fellow student and I co-founded a company. Through this venture, we had the chance to work on several exciting projects! If you're interested, check out <a href='https://www.hiisi.digital/'>hiisi.digital</a>.
+                During my studies, a fellow student and I co-founded a company. Through this venture, we had the chance to work on several exciting projects! If you&apos;re interested, check out <a href='https://www.hiisi.digital/'>hiisi.digital</a>.
               </h3>
               
               
@@ -103,15 +60,15 @@ function ScrollSection() {
 
           <div className="scroll-section third-section">
             <div className="section-3">
-              <h2>What i know</h2>
-              <h3>For me, it's not just about mastering specific programming languages or technologies; it's about embracing the entirety of web development. I've garnered a wealth of experience across a range of technologies over the years.</h3>
+              <h2>What I know</h2>
+              <h3>For me, it&apos;s not just about mastering specific programming languages or technologies; it&apos;s about embracing the entirety of web development. I&apos;ve garnered a wealth of experience across a range of technologies over the years.</h3>
             </div>
             <div className="section-3">
               <h2 className="title">Technologies and tools</h2>
               <h3>
-                Over the years, my journey as a web developer has been both vast and fulfilling. I've mastered the art of creating seamless user interfaces using tools like HTML, CSS, JavaScript, and React. Venturing further, PHP, Next.js, TailwindCSS, and WordPress became trusted companions in my toolkit, allowing me to shape visions into digital realities.
+                Over the years, my journey as a web developer has been both vast and fulfilling. I&apos;ve mastered the art of creating seamless user interfaces using tools like HTML, CSS, JavaScript, and React. Venturing further, PHP, Next.js, TailwindCSS, and WordPress became trusted companions in my toolkit, allowing me to shape visions into digital realities.
               <br/> <br/>
-                Of course, I'm accustomed to using version control in my projects, and in addition, I've had a glimpse into the world of databases and servers.
+                Of course, I&apos;m accustomed to using version control in my projects, and in addition, I&apos;ve had a glimpse into the world of databases and servers.
               </h3>
             </div>
           </div>
