2a2dc25f898a04493177a38b69dc9cc87207c6cf
villetomela <ville.toimela@bermuda.fi>
2024-01-11 16:39:56 +0200

    Deleted some project from portfolio for now.

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -4,48 +4,48 @@ import Project from '../components/project';
 
 const ProjectGallery = forwardRef((props, ref) => {
   const projects = [
-    {
-      title1: "Magister",
-      title2: "Solutions",
-      src: "project-magister.png",
-      href: "https://data.magister.fi/"
-    },
-    {
-      title1: "Kyocera",
-      title2: "Technologies",
-      src: "project-kyocera.png",
-      href: "https://kyoceratechnologies.com/"
-    },
-    {
-      title1: "Roihu",
-      title2: "Law",
-      src: "project-roihu.png",
-      href: "https://roihulaw.fi/"
-    },
-    {
-      title1: "Tammermatic",
-      title2: "",
-      src: "project-tammermatic.png",
-      href: "https://tammermatic.com/"
-    },
-    {
-      title1: "Kontukoti",
-      title2: "",
-      src: "project-kontu.png",
-      href: "https://kontukoti.fi/"
-    },
-    {
-      title1: "Sleipner",
-      title2: "",
-      src: "project-sleipner.png",
-      href: "https://sleipner.com/"
-    },
-    {
-      title1: "Sisuline",
-      title2: "",
-      src: "project-sisuline.png",
-      href: "https://sisuline.fi/"
-    },
+    // {
+    //   title1: "Magister",
+    //   title2: "Solutions",
+    //   src: "project-magister.png",
+    //   href: "https://data.magister.fi/"
+    // },
+    // {
+    //   title1: "Kyocera",
+    //   title2: "Technologies",
+    //   src: "project-kyocera.png",
+    //   href: "https://kyoceratechnologies.com/"
+    // },
+    // {
+    //   title1: "Roihu",
+    //   title2: "Law",
+    //   src: "project-roihu.png",
+    //   href: "https://roihulaw.fi/"
+    // },
+    // {
+    //   title1: "Tammermatic",
+    //   title2: "",
+    //   src: "project-tammermatic.png",
+    //   href: "https://tammermatic.com/"
+    // },
+    // {
+    //   title1: "Kontukoti",
+    //   title2: "",
+    //   src: "project-kontu.png",
+    //   href: "https://kontukoti.fi/"
+    // },
+    // {
+    //   title1: "Sleipner",
+    //   title2: "",
+    //   src: "project-sleipner.png",
+    //   href: "https://sleipner.com/"
+    // },
+    // {
+    //   title1: "Sisuline",
+    //   title2: "",
+    //   src: "project-sisuline.png",
+    //   href: "https://sisuline.fi/"
+    // },
     {
       title1: "Hiisi",
       title2: "Digital",
