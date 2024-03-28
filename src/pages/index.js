069dda6c9b940ad218ad4647de5af49b0ab24125
Ville Toimela <ville.toimela@bermuda.fi>
2024-03-28 14:42:17 +0200

    Added ZoomParallax.js and some minor style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,7 @@ import TextSection from '@/components/TextSection';
 import FloatingGallery from '@/components/FloatingGallery';
 import WorksPeek from '@/components/WorksPeek';
 import ProjectGallery from '@/components/ProjectGallery';
+import ZoomParallax from '@/components/ZoomParallax';
 
 export default function Home() {
 
@@ -29,15 +30,15 @@ export default function Home() {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect( () => {
+  useEffect(() => {
     (
       async () => {
 
-          setTimeout( () => {
-            setIsLoading(false);
-            document.body.style.cursor = 'default'
-            window.scrollTo(0,0);
-          }, 2500)
+        setTimeout(() => {
+          setIsLoading(false);
+          document.body.style.cursor = 'default'
+          window.scrollTo(0, 0);
+        }, 2500)
       }
     )()
   }, [])
@@ -63,12 +64,13 @@ export default function Home() {
 
       <Header projects={projects} home={home} contact={contact} textsection={textsection} />
       <Hero ref={home} />
+      <ZoomParallax />
       <TextSection ref={textsection}
-        title = "Who am i"
-        content ="Hello! I'm a 28-years-old eager Web Developer with about four years of active coding experience. While I've always been intrigued by programming, I officially embarked on my learning journey only four years ago. During this time, I learned many different skills and made some digital ideas real! I've logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate atleast an equal number of hours in coding and continue expanding my knowledge."
+        title="Who am i"
+        content="Hello! I'm a 28-years-old eager Web Developer with about four years of active coding experience. While I've always been intrigued by programming, I officially embarked on my learning journey only four years ago. During this time, I learned many different skills and made some digital ideas real! I've logged tens of thousands of hours playing video games. The passion and commitment I once dedicated to gaming have now transitioned into software development. My aim is to accumulate atleast an equal number of hours in coding and continue expanding my knowledge."
       />
       <TextSection
-        title = "What i do"
+        title="What i do"
         content={
           <>
             After an intensive two-year Software Developer training, I had the privilege of working at one of Finland&apos;s most influential marketing agencies. There, I&apos;ve been involved in creating various websites tailored to meet the specific needs of clients. Most of these projects involved building custom WordPress themes. In addition to coding, this role has provided me with invaluable experience in teamwork and interacting effectively with clients.
@@ -77,20 +79,20 @@ export default function Home() {
             During my studies, my brother and I co-founded a company. Through this venture, we had the chance to work on several exciting projects! If you&apos;re interested, check out <a href="https://www.hiisi.digital/" target="_blank" rel="noopener noreferrer">hiisi.digital</a>.
           </>
         }
-        />
-      <TitleSection title ="Sneak peek to some of my projects" />
+      />
+      {/* <TitleSection title ="Sneak peek to some of my projects" /> */}
       <WorksPeek />
-      <TitleSection textAlignRight={true} title ="Link to " link="https://github.com/villetoimela" linkTitle="> github" />
+      {/* <TitleSection textAlignRight={true} title ="Link to " link="https://github.com/villetoimela" linkTitle="> github" /> */}
       <TextSection
-        title = "What i know"
-        content ="For me, it's not just about mastering specific programming languages or technologies; it's about embracing the entirety of web development. I've garnered a wealth of experience across a range of technologies over the years."
+        title="What i know"
+        content="For me, it's not just about mastering specific programming languages or technologies; it's about embracing the entirety of web development. I've garnered a wealth of experience across a range of technologies over the years."
       />
       <TextSection
-        title = "Techs and tools"
-        content ="Over the years, my journey as a web developer has been both vast and fulfilling. I've mastered the art of creating seamless user interfaces using tools like HTML, CSS, JavaScript, and React. Venturing further, PHP, Next.js, TailwindCSS, and WordPress became trusted companions in my toolkit, allowing me to shape visions into digital realities.
+        title="Techs and tools"
+        content="Over the years, my journey as a web developer has been both vast and fulfilling. I've mastered the art of creating seamless user interfaces using tools like HTML, CSS, JavaScript, and React. Venturing further, PHP, Next.js, TailwindCSS, and WordPress became trusted companions in my toolkit, allowing me to shape visions into digital realities.
           Of course, I'm accustomed to using version control in my projects, and in addition, I've had a glimpse into the world of databases and servers."
       />
-      <TitleSection href="work" title ="Some of my latest works" />
+      {/* <TitleSection href="work" title ="Some of my latest works" /> */}
       <ProjectGallery ref={projects} />
       {/* <Projects1 /> */}
       {/* <Projects2 /> */}
@@ -98,7 +100,7 @@ export default function Home() {
       {/* <ScrollSection /> */}
       {/* <ParallaxSection /> */}
       {/* <ScrollSectionShort /> */}
-      <TitleSection title ="Tech & tools i have been using lately" />
+      <TitleSection title="Tech & tools i have been using lately" />
       <Technologies />
       <Footer ref={contact} />
       <div className="noise"></div>
