f0325625871cdb76267e0e6b2f0680f52e16abc5
villetomela <ville.toimela@bermuda.fi>
2024-06-17 11:53:27 +0300

    Moved title and meta tags from document.js to app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.scss'
+import '@/styles/globals.scss';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Ville Toimela - Web Developer</title>
+        <meta name="description" content="I am a passionate web developer with some experience in creating diverse websites and web applications." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
