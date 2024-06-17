ac87af510542b30e706666fc6896dcef486a0003
Ville Toimela <ville.toimela@bermuda.fi>
2024-06-17 12:10:29 +0300

    Deleted unused components :D

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import React, { useState, useEffect, useRef } from 'react';
 import Lenis from '@studio-freight/lenis';
 import { AnimatePresence } from 'framer-motion';
 import Hero from '@/components/Hero'
-import ScrollSection from '@/components/ScrollSection'
 import Footer from '@/components/Footer'
-import Projects1 from '@/components/Projects1'
-import Projects2 from '@/components/Projects2'
 import TitleSection from '@/components/TitleSection'
 import Preloader from '@/components/Preloader'
-import ParallaxSection from '@/components/ParallaxSection';
-import InfiniteScroll from '@/components/InfiniteScroll';
 import Header from '@/components/Header';
-import ScrollSectionShort from '@/components/ScrollSectionShort';
 import Technologies from '@/components/Technologies';
 import TextSection from '@/components/TextSection';
-import FloatingGallery from '@/components/FloatingGallery';
 import WorksPeek from '@/components/WorksPeek';
 import ProjectGallery from '@/components/ProjectGallery';
 import ZoomParallax from '@/components/ZoomParallax';
